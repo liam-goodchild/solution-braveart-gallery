6002e390e06193596ee0f29028fe2a83bb0df7f9
Liam Goodchild <liamgoodchild12@hotmail.co.uk>
2026-04-19 17:40:08 +0100

    Fix CSP violations and Stripe image URL handling
    Add Google Fonts to style-src and font-src, allow data: URIs in img-src,
    and prefix bare Stripe file link tokens with the files.stripe.com base URL

--- functions/src/functions/getArtworks.js
+++ functions/src/functions/getArtworks.js
@@ -21,7 +21,11 @@ app.http("getArtworks", {
         name: p.name,
         description: p.description,
         price: p.default_price.unit_amount,
-        imageUrl: p.images.length > 0 ? p.images[0] : null,
+        imageUrl: p.images.length > 0
+          ? (p.images[0].startsWith("http")
+              ? p.images[0]
+              : `https://files.stripe.com/links/${p.images[0]}`)
+          : null,
       }));
 
     return {
