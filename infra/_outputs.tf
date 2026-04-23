f5bac103391a784d1c53577f66f2fca2ee632933
Liam Goodchild <liamgoodchild12@hotmail.co.uk>
2026-04-23 23:55:32 +0100

    Refactor DNS to Cloudflare

--- infra/_outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cloudflare_nameservers" {
-  description = "Cloudflare nameservers to configure manually at the registrar after the zone is created."
-  value       = { for zone_name, zone in cloudflare_zone.public : zone_name => zone.name_servers }
-}
